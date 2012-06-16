c59a962709aae6b4722c0a8d80f834989d7a2860
Nick Reid <nickreid@nickreid.com>
2012-06-15 17:38:51 -0700

    added database settings

--- pchsi_recommends/settings.py
+++ pchsi_recommends/settings.py
@@ -9,10 +9,12 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
+import os
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': os.getcwd()+'/database.db',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -119,9 +121,10 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+	'pchsi_recommends.recommendations'
 )
 
 # A sample logging configuration. The only tangible logging
