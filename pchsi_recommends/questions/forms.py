582c22bff8df0191f005d164262929aa5babcb48
Nick Reid <nickreid@nickreid.com>
2012-06-20 15:01:11 -0700

    got question form mostly right

--- pchsi_recommends/questions/forms.py
+++ pchsi_recommends/questions/forms.py
@@ -13,6 +13,44 @@ gender_current = (
     ('transfemale', 'Trans Female'),
 )
 
+sexual_orientation = (
+	('bisexual','BiSexual'),
+	('gay','Gay'),
+	('lesbian','Lesbian'),
+	('straight','Straight'),
+)
+
+health_conditions = (
+	('pregnant','Pregnant'),
+	('tb','Tuberculosis'),
+	('hepb','Hepatitis B'),
+	('hepc','Hepatitis C'),
+	('hiv','HIV/AIDS'),
+)
+
 
 class PatientForm(forms.Form):
-    gender_at_birth = forms.ChoiceField(widget = forms.RadioSelect,choices = gender_at_birth)
+	gender_at_birth = forms.ChoiceField(
+		label = 'What was your gender at birth',
+		widget = forms.RadioSelect,
+		choices = gender_at_birth
+		)
+	
+	gender_current = forms.ChoiceField(
+		label = 'What is your gender',
+		widget = forms.RadioSelect,
+		choices = gender_current
+		)
+	
+	sexual_orientation = forms.ChoiceField(
+		label = 'What is your sexual orientation',
+		widget = forms.RadioSelect,
+		choices = sexual_orientation
+		)
+	
+	health_conditions = forms.MultipleChoiceField(
+		required = False,
+		label = 'Do you have any of the following health conditions',
+		widget = forms.CheckboxSelectMultiple,
+		choices = health_conditions
+		)
--- pchsi_recommends/questions/views.py
+++ pchsi_recommends/questions/views.py
@@ -7,11 +7,15 @@ from django import forms
 from django.template import RequestContext
 
 from forms import PatientForm
+from pchsi_recommends.recommendations.models import *
 
 def patient_form(request):
+	form = PatientForm()
 	if request.method == 'POST':
 		form = PatientForm(request.POST)
 		if form.is_valid():
+			answers = form.cleaned_data
+			# map answers to populations
+			# loop through screens and pull recommendations
 			return render_to_response('questions/recommendations.html')
-	form = PatientForm()
 	return render_to_response('questions/form.html',{'form':form},context_instance=RequestContext(request))
