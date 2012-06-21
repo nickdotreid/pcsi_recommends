b41273b596d5973774875a12aef1139a1b673dbf
Nick Reid <nickreid@nickreid.com>
2012-06-21 13:26:57 -0700

    made basic dynamic questionare work

--- pchsi_recommends/questions/admin.py
+++ pchsi_recommends/questions/admin.py
@@ -1,5 +1,12 @@
 from pchsi_recommends.questions.models import Question,Answer
 from django.contrib import admin
 
-admin.site.register(Question)
+class AnswerInline(admin.StackedInline):
+	model = Answer
+	extra = 2
+
+class QuestionAdmin(admin.ModelAdmin):
+	inlines = [AnswerInline]
+
+admin.site.register(Question,QuestionAdmin)
 admin.site.register(Answer)
--- pchsi_recommends/questions/views.py
+++ pchsi_recommends/questions/views.py
@@ -8,6 +8,7 @@ from django.template import RequestContext
 
 from forms import PatientForm
 from pchsi_recommends.recommendations.models import *
+from pchsi_recommends.questions.models import *
 
 def patient_form(request):
 	form = PatientForm()
@@ -19,3 +20,30 @@ def patient_form(request):
 			# loop through screens and pull recommendations
 			return render_to_response('questions/recommendations.html')
 	return render_to_response('questions/form.html',{'form':form},context_instance=RequestContext(request))
+
+def dynamic_form(request):
+	if request.method == 'POST':
+		populations = []
+		for key in request.POST:
+			if key != 'csrfmiddlewaretoken':
+				question = Question.objects.filter(id=key)[0]
+				if question:
+					answer = question.answer_set.filter(id=request.POST[key])[0]
+					if answer:
+						for population in answer.populations.all():
+							if population not in populations:
+								populations.append(population)
+		recommendations = []
+		if len(populations) > 0:
+			for screen in Screen.objects.all():
+				recommend = False
+				for recommendation in screen.recommendation_set.all():
+					for population in recommendation.populations.all():
+						if population in populations:
+							if not recommend or recommend.weight > recommendation.weight:
+								recommend = recommendation
+				if recommend:
+					recommendations.append(recommend)
+		return render_to_response('questions/recommendations.html',{'recommendations':recommendations})
+	questions = Question.objects.all()
+	return render_to_response('questions/dynamic.html',{'questions':questions},context_instance=RequestContext(request))
