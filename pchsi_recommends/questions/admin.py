c45e50b467258cc8b5be30aa910b9720d192493e
Nick Reid <nickreid@nickreid.com>
2012-06-21 09:25:28 -0700

    added question/answer models to dynamically create questions and tie to populations

---
+++ pchsi_recommends/questions/admin.py
@@ -0,0 +1,5 @@
+from pchsi_recommends.questions.models import Question,Answer
+from django.contrib import admin
+
+admin.site.register(Question)
+admin.site.register(Answer)
--- pchsi_recommends/questions/models.py
+++ pchsi_recommends/questions/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from pchsi_recommends.recommendations.models import Population
 
-# Create your models here.
+class Question(models.Model):
+	"""(Question description)"""
+	
+	text = models.TextField(blank=True)
+	
+	class Admin:
+		pass
+
+	def __unicode__(self):
+		return self.text
+		
+class Answer(models.Model):
+	
+	question = models.ForeignKey(Question)
+	populations = models.ManyToManyField(Population)
+	text = models.TextField(blank=True)
+	
+	def __unicode__(self):
+		return self.question.text + ": " + self.text
