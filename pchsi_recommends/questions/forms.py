e33428b6afc5e7b201562e0e14d9b738573fcf08
Nick Reid <nickreid@nickreid.com>
2012-06-22 14:18:08 -0700

    no luck

--- pchsi_recommends/questions/forms.py
+++ pchsi_recommends/questions/forms.py
@@ -3,16 +3,17 @@ from pchsi_recommends.questions.models import *
 		
 def make_question_form(questionnaire_id):
 	field_list = []
-	fields = {}
 	questionnaire = Questionnaire.objects.filter(id=questionnaire_id)[0]
 	if questionnaire:
 		for question in questionnaire.question_set.order_by('weight'):
 			answers = []
 			for answer in question.answer_set.all():
 				answers.append((answer.id,answer.text))
-			fields[question.short] = forms.ChoiceField(
-					widget = forms.RadioSelect,
-					label = question.text,
-					choices = answers
-				)
-	return type('QuestionForm',(forms.BaseForm,),{'base_fields':fields})
+			field_list.append((question.short, forms.ChoiceField(
+						widget = forms.RadioSelect,
+						label = question.text,
+						choices = answers
+					)))
+	return type('QuestionForm',(forms.BaseForm,),{
+		'base_fields':dict(field_list),
+		})
