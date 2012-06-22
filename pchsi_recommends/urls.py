47046ea2c14f345647c9450c7e4c4059497480e6
Nick Reid <nickreid@nickreid.com>
2012-06-22 11:20:27 -0700

    changed url structure for testing and building

--- pchsi_recommends/urls.py
+++ pchsi_recommends/urls.py
@@ -14,5 +14,6 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-	url(r'^$', include('pchsi_recommends.questions.urls')),
+	url(r'^questions/$', include('pchsi_recommends.questions.urls')),
+	url(r'^recomendations/$', include('pchsi_recommends.recommendations.urls')),
 )
