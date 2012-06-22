1793317e44124723987769eb06ea7f63e1be0cd3
Nick Reid <nickreid@nickreid.com>
2012-06-22 11:52:19 -0700

    added form model

--- pchsi_recommends/questions/models.py
+++ pchsi_recommends/questions/models.py
@@ -1,14 +1,23 @@
 from django.db import models
 from pchsi_recommends.recommendations.models import Population
 
+class Form(models.Model):
+	''' Collects questions and has description '''
+	title = models.CharField(blank=True, max_length=100)
+	directions = models.TextField(blank=True)
+	
+	def __unicode__(self):
+		return self.title
+
 class Question(models.Model):
 	"""(Question description)"""
 	
-	text = models.TextField(blank=True)
+	form = models.ForeignKey(Form)
+	text = models.CharField(max_length=250)
+	description = models.TextField(blank=True)
 	
-	class Admin:
-		pass
-
+	weight = models.IntegerField(blank=True, null=True)
+	
 	def __unicode__(self):
 		return self.text
 		
@@ -16,7 +25,9 @@ class Answer(models.Model):
 	
 	question = models.ForeignKey(Question)
 	populations = models.ManyToManyField(Population)
-	text = models.TextField(blank=True)
+	
+	text = models.CharField(max_length=250)
+	description = models.TextField(blank=True)
 	
 	def __unicode__(self):
 		return self.question.text + ": " + self.text
