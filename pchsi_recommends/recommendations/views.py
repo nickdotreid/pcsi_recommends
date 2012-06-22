20afcd49ac6aa7bc5e9d86a58ea9ef7e53b0fb3d
Nick Reid <nickreid@nickreid.com>
2012-06-22 11:19:38 -0700

    made test view to check recommendations for specific populations

--- pchsi_recommends/recommendations/views.py
+++ pchsi_recommends/recommendations/views.py
@@ -1,5 +1,46 @@
-# Create your views here.
+from django.template import Context, loader
+from django.shortcuts import render_to_response, get_object_or_404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+
+from django import forms
+from django.template import RequestContext
+
 from pchsi_recommends.recommendations.models import *
+
+def make_population_form():
+	populations = []
+	for population in Population.objects.all():
+		populations.append((population.id,population.name))
+	fields = {}
+	fields['populations'] = forms.MultipleChoiceField(
+		required = False,
+		label = 'Select Populations',
+		widget = forms.CheckboxSelectMultiple,
+		choices = populations
+		)
+	return type('PopulationForm',(forms.BaseForm,),{'base_fields':fields})
+	
+
+def population_test_form(request):
+	form = make_population_form()()
+	if request.method == 'POST':
+		form = make_population_form()(request.POST)
+		if form.is_valid():
+			answers = form.cleaned_data
+			populations = []
+			for pop_id in answers['populations']:
+				population = Population.objects.filter(id = pop_id)[0]
+				if population:
+					populations.append(population)
+			return render_to_response('recommendations/population_test_form.html',{
+				'form':form,
+				'recommendations':populations_to_recomendations(populations)
+				},context_instance=RequestContext(request))
+	return render_to_response('recommendations/population_test_form.html',{
+		'form':form,
+		'recommendations':[]
+		},context_instance=RequestContext(request))
+	
 
 def populations_to_recomendations(populations):
 	recommendations = []
