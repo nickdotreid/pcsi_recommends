d463a22a9c9566f5f68e6627dae476a4efd929d3
Nick Reid <nickreid@nickreid.com>
2012-06-26 08:40:57 -0700

    removed default db set up

--- pchsi_recommends/settings.py
+++ pchsi_recommends/settings.py
@@ -11,17 +11,6 @@ MANAGERS = ADMINS
 
 import os
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': os.getcwd()+'/database.db',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
-
 import dj_database_url
 DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
 
