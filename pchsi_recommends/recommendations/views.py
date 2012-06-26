344494e8d656f8b860e93ae6d21dc5eac82da4e4
Nick Reid <nickreid@nickreid.com>
2012-06-26 09:51:10 -0700

    septerated out screen for population

--- pchsi_recommends/recommendations/views.py
+++ pchsi_recommends/recommendations/views.py
@@ -46,12 +46,16 @@ def populations_to_recomendations(populations):
 	recommendations = []
 	if len(populations) > 0:
 		for screen in Screen.objects.all():
-			recommend = False
-			for recommendation in screen.recommendation_set.all():
-				for population in recommendation.populations.all():
-					if population in populations:
-						if not recommend or recommend.weight > recommendation.weight:
-							recommend = recommendation
+			recommend = get_recommendation_for(screen,populations)
 			if recommend:
 				recommendations.append(recommend)
 	return recommendations
+
+def get_recommendation_for(screen,populations):
+	recommend = False
+	for recommendation in screen.recommendation_set.all():
+		for population in recommendation.populations.all():
+			if population in populations:
+				if not recommend or recommend.weight > recommendation.weight:
+					recommend = recommendation
+	return recommend
