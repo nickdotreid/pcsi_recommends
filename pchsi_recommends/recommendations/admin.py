25f0b35f18510bee262af9233d4826f6743744f6
Nick Reid <nickreid@nickreid.com>
2012-07-03 15:52:37 -0700

    added recommendations to screen admin

--- pchsi_recommends/recommendations/admin.py
+++ pchsi_recommends/recommendations/admin.py
@@ -1,7 +1,18 @@
 from pchsi_recommends.recommendations.models import Recommendation, Screen, Population, Population_Relationship
 from django.contrib import admin
 
-admin.site.register(Screen)
+
+class RecommendationInline(admin.TabularInline):
+	model = Recommendation
+	fields = ("weight","frequency","not_recommended")
+	sortable_field_name = "weight"
+	extra = 1
+
+class ScreenAdmin(admin.ModelAdmin):
+	inlines = [RecommendationInline]
+
+
+admin.site.register(Screen,ScreenAdmin)
 admin.site.register(Population)
 
 class PopulationRelationshipInline(admin.TabularInline):
