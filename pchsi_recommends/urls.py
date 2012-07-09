a4a050cc866a2330ce9479f910f85fadc370bdf6
Nick Reid <nickreid@nickreid.com>
2012-07-09 11:31:44 -0700

    updated copy and links

--- pchsi_recommends/urls.py
+++ pchsi_recommends/urls.py
@@ -6,6 +6,6 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 #	url(r'^questions/', include('pchsi_recommends.questions.urls')),
-	url(r'^recs/', include('pchsi_recommends.recommendations.urls')),
+	url(r'^providers/', include('pchsi_recommends.recommendations.urls')),
 	url(r'^', include('pchsi_recommends.questions.urls')),
 )
