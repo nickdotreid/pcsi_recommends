73ff39b4eb578ac22a15aa3f865b0ebba8217e2d
Nick Reid <nickreid@nickreid.com>
2012-07-09 10:44:38 -0700

    made recommendations two page process

--- pchsi_recommends/questions/views.py
+++ pchsi_recommends/questions/views.py
@@ -30,7 +30,7 @@ def base_question_form(request):
 			answers = DotExpandedDict(form.cleaned_data)
 			request.session['age'] = answers_to_age(answers)
 			request.session['populations'] = answers_to_populations(answers)
-			return redirect('/more')
+			return redirect('/recommendations')
 	return render_to_response('questions/form.html',{
 		'form':form,
 		},context_instance=RequestContext(request))
@@ -60,7 +60,21 @@ def show_recommendations(request):
 		age = request.session['age']
 	if 'populations' in request.session:
 		populations = request.session['populations']
-	reset_session(request)
+	QuestionForm = make_additional_question_form(request.session['populations'],request.session['age'],request.session['questions_asked'])
+	form = QuestionForm()
+	if request.method == 'POST':
+		form = QuestionForm(request.POST)
+		if form.is_valid():
+			answers = DotExpandedDict(form.cleaned_data)
+			request.session['age'] = answers_to_age(answers)
+			request.session['populations'] = list(set(itertools.chain(request.session['populations'],answers_to_populations(answers))))
+			request.session['questions_asked'] = list(set(itertools.chain(request.session['questions_asked'],answers['questions'].keys())))
+			QuestionForm = make_additional_question_form(request.session['populations'],request.session['age'],request.session['questions_asked'])
+			form = QuestionForm(request.POST)
+	if len(form.fields)<1:
+		form = False
+#	reset_session(request)
 	return render_to_response('questions/responses.html',{
-		'recommendations':populations_to_recomendations(populations,age)
+		'recommendations':populations_to_recomendations(populations,age),
+		'form':form,
 		},context_instance=RequestContext(request))
