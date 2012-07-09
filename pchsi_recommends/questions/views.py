4f6d1c14b58b76391a5c20df5dd0fceeb8a56595
Nick Reid <nickreid@nickreid.com>
2012-07-09 11:43:25 -0700

    checked anwsers[questions] before using

--- pchsi_recommends/questions/views.py
+++ pchsi_recommends/questions/views.py
@@ -46,7 +46,8 @@ def show_recommendations(request):
 			answers = DotExpandedDict(form.cleaned_data)
 			request.session['age'] = answers_to_age(answers)
 			request.session['populations'] = list(set(itertools.chain(request.session['populations'],answers_to_populations(answers))))
-			request.session['questions_asked'] = list(set(itertools.chain(request.session['questions_asked'],answers['questions'].keys())))
+			if 'questions' in answers:
+				request.session['questions_asked'] = list(set(itertools.chain(request.session['questions_asked'],answers['questions'].keys())))
 			QuestionForm = make_additional_question_form(request.session['populations'],request.session['age'],request.session['questions_asked'])
 			form = QuestionForm(request.POST)
 	if len(form.fields)<1:
