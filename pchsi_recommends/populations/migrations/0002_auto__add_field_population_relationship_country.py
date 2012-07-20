04fd4b5549d6ea75e968c5cda5a835fa0c20a29c
Nick Reid <nickreid@nickreid.com>
2012-07-20 12:42:44 -0700

    forgot to add migrations

---
+++ pchsi_recommends/populations/migrations/0002_auto__add_field_population_relationship_country.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'Population_Relationship.country'
+        db.add_column('populations_population_relationship', 'country',
+                      self.gf('django_countries.fields.CountryField')(default=None, max_length=2),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'Population_Relationship.country'
+        db.delete_column('populations_population_relationship', 'country')
+
+
+    models = {
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'populations.population': {
+            'Meta': {'object_name': 'Population'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '120'}),
+            'short': ('django.db.models.fields.CharField', [], {'max_length': '25'})
+        },
+        'populations.population_relationship': {
+            'Meta': {'object_name': 'Population_Relationship'},
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'country': ('django_countries.fields.CountryField', [], {'max_length': '2'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'inclusive': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'max_age': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'min_age': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'object_id': ('django.db.models.fields.PositiveIntegerField', [], {}),
+            'populations': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['populations.Population']", 'symmetrical': 'False', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['populations']
