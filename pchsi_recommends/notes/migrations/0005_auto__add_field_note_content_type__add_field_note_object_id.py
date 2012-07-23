7dd44af00a710b437de881f91e4a186a05f16323
Nick Reid <nickreid@nickreid.com>
2012-07-23 11:27:19 -0700

    added generic relation field to notes

---
+++ pchsi_recommends/notes/migrations/0005_auto__add_field_note_content_type__add_field_note_object_id.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'Note.content_type'
+        db.add_column('notes_note', 'content_type',
+                      self.gf('django.db.models.fields.related.ForeignKey')(to=orm['contenttypes.ContentType'], null=True, blank=True),
+                      keep_default=False)
+
+        # Adding field 'Note.object_id'
+        db.add_column('notes_note', 'object_id',
+                      self.gf('django.db.models.fields.PositiveIntegerField')(null=True, blank=True),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'Note.content_type'
+        db.delete_column('notes_note', 'content_type_id')
+
+        # Deleting field 'Note.object_id'
+        db.delete_column('notes_note', 'object_id')
+
+
+    models = {
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'notes.note': {
+            'Meta': {'object_name': 'Note'},
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']", 'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'object_id': ('django.db.models.fields.PositiveIntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'text': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '100', 'blank': 'True'}),
+            'weight': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'})
+        },
+        'populations.population': {
+            'Meta': {'object_name': 'Population'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '120'}),
+            'short': ('django.db.models.fields.CharField', [], {'max_length': '25'})
+        },
+        'populations.population_relationship': {
+            'Meta': {'object_name': 'Population_Relationship'},
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'country': ('django_countries.fields.CountryField', [], {'max_length': '2', 'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'inclusive': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'max_age': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'min_age': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'object_id': ('django.db.models.fields.PositiveIntegerField', [], {}),
+            'populations': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['populations.Population']", 'symmetrical': 'False', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['notes']
--- pchsi_recommends/notes/models.py
+++ pchsi_recommends/notes/models.py
@@ -5,10 +5,14 @@ from django.contrib.contenttypes import generic
 from pchsi_recommends.populations.models import Population_Relationship
 
 class Note(models.Model):
-	""" Reusable Notes Field """
+	""" Reusable Notes Field That can be filtered by population types """
 	title = models.CharField(blank=True, max_length=100)
 	text = models.TextField(blank=True)
 	
 	weight = models.IntegerField(blank=True, null=True)
 	
 	populations = generic.GenericRelation(Population_Relationship)
+	
+	content_type = models.ForeignKey(ContentType, blank=True, null=True)
+	object_id = models.PositiveIntegerField(blank=True, null=True)
+	content_object = generic.GenericForeignKey('content_type','object_id')
