ebc67fd7abd00f09876893fc85bf051b94df8df8
Nick Reid <nickreid@nickreid.com>
2012-07-23 15:24:13 -0700

    updaed some copy

--- pchsi_recommends/provider_form/urls.py
+++ pchsi_recommends/provider_form/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('pchsi_recommends.provider_form.views',
 	url(r'^$','home_page'),
-	url(r'^form$','population_test_form'),
+	url(r'^form/$','population_test_form'),
 )
