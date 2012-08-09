41e65da70f948f228b8cf56f48ac5eb750281f0f
Nick Reid <nickreid@nickreid.com>
2012-08-09 15:49:12 -0700

    highlight selected drop down now works

--- pchsi_recommends/questions/highlightselectwidget.py
+++ pchsi_recommends/questions/highlightselectwidget.py
@@ -2,6 +2,7 @@ from django.forms import Select
 from django.utils.encoding import force_unicode
 from django.utils.safestring import mark_safe
 from django.forms.widgets import flatatt
+from itertools import chain
 
 class HighlightedSelect(Select):
 	"""A widget that displays JSON Key Value Pairs
@@ -26,17 +27,29 @@ class HighlightedSelect(Select):
 
 	def render(self, name, value, attrs=None, choices=()):
 		if value is None: value = ''
-		highlight_output = ""
+		highlight_vals = []
+		output = []
 		for val,label in self.highlighted:
-			highlight_output = highlight_output + self.render_radio_input(name+'_highlight',val,label,value,attrs)
-		final_attrs = self.build_attrs(attrs, name=name)
-		output = [u'<select%s>' % flatatt(final_attrs)]
-		options = self.render_options(choices, [value])
+			highlight_vals.append(val)
+			output.append(self.render_radio_input(name+'_highlight',val,label,value,attrs))
+		for val,label in chain(self.choices,choices):
+			if val != '' and val not in highlight_vals and val == value:
+				highlight_vals.append(val)
+				output.append(self.render_radio_input(name+'_highlight',val,label,value,attrs))
+		final_attrs = self.build_attrs(attrs, name=name)		
+		output.append(u'<select%s>' % flatatt(final_attrs))
+		_choices = []
+		if len(choices) < 1:
+			choices = self.choices
+		for val,label in choices:
+			if val not in highlight_vals:
+				_choices.append((val,label))
+		self.choices = _choices # i don't love this line
+		options = self.render_options([], [value])
 		if options:
 			output.append(options)
 		output.append(u'</select>')
-		highlight_output = highlight_output + u'\n'.join(output)
-		return mark_safe(highlight_output)
+		return mark_safe(u'\n'.join(output))
 	
 	def render_radio_input(self,name,value,label,selected_value,attrs):
 		highlight_attrs = self.build_attrs(attrs, name=name, value=value)
