3698ded2b413b21e80471886e71ffe26a5e8d6ad
Nick Reid <nickreid@nickreid.com>
2012-09-06 16:14:32 -0700

    removing ajax components from questions app

--- pchsi_recommends/questions/urls.py
+++ pchsi_recommends/questions/urls.py
@@ -5,7 +5,4 @@ urlpatterns = patterns('pchsi_recommends.questions.views',
 	url(r'^$','initial_page'),
 	url(r'^recommendations$','recommendations_page'),
 	url(r'^change$','all_questions'),
-	url(r'^ajax/save$','ajax_save_answer'),
-	url(r'^ajax/questions$','ajax_get_questions'),
-	url(r'^ajax/update$','ajax_get_recommendations'),
 )
